a5cf6b83e38ffe5d807854aac33518f49acd333d
Dinara Aldarova <dinara_aldarova@mail.ru>
2023-03-17 21:30:49 +0500

    first commit

---
+++ dogs.js
@@ -0,0 +1,122 @@
+var dogs = [
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/13-1.jpg",
+        "description": "Лола уже не молода. Но это не значит ничего плохого — в меру спокойная, понятливая и самостоятельная собачка не уже будет хулиганить, как щенок, и станет прекрасным компаньоном.",
+        "name": "Лола",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/13-1-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/1.jpg",
+        "description": "Уля крупная, мощная, сильная и энергичная. Для своих — цветочек, для чужих — годзилла. Умная и отличная охрана для вашего имущества, прекрасный компаньон и ласковый друг для близких.",
+        "name": "Уля",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/1-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/45.jpg",
+        "description": "Шницель — метис шнауцера. Умный парень отличного квартирного размера. Понятливый, ласковый и добрый. Умеет ходить на поводке и не доставить много хлопот.",
+        "name": "Шницель",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/45-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/29.jpg",
+        "description": "Рыжая лисичка Жуля очень рада любому вниманию. Привязчивая, ласковая, послушная собачка, умеет ходить на поводке и заглядывать в глаза.",
+        "name": "Жуля",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/29-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/3.jpg",
+        "description": "С энергичной Карамелькой будет отличной заняться дрессировкой. А если её шикарную шерсть отмыть и вычесать... Загляденье будет, а не собака.",
+        "name": "Карамелька",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/3-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/27_2.jpg",
+        "description": "Рой — статный, красивый и серьёзный парень. Отлично поддаётся дрессировке, смышлёный. Станет отличным охранником и компаньоном.",
+        "name": "Рой",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/27_2-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/25.jpg",
+        "description": "Барби — просто куколка. Безумно ласковая и добрая собака. Она очень нуждается в общении, внимании и ласке. Будет прекрасным домашним любимцем.",
+        "name": "Барби",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/25-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/22.jpg",
+        "description": "Дана никогда не унывает и не даст унывать и вам. Любит всех и даже в условиях приюта умудрилась выучить некоторые команды. Золото, а не собака!",
+        "name": "Дана",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/22-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/20.jpg",
+        "description": "Нежная Зефирка только учится доверять человеку, и прогресс на лицо! Небольшая, очень ласковая и трогательная собачка.",
+        "name": "Зефирка",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/20-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/19.jpg",
+        "description": "Жози, Миша и их куратор Женя на прогулке. Замечательные, послушные собаки. Просто им нужно немножко внимания и любви!",
+        "name": "Жози и Миша",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/19-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/16.jpg",
+        "description": "Полечке ещё нет и года. Ласковая, послушная и добрая собака. Участвовала в выставке собак, но до сих пор ждёт своего хозяина.",
+        "name": "Поля",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/16-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/15.jpg",
+        "description": "Красавец Топаз. Шоколадный окрас, лаечный типаж и нордический нрав. Готов доверять не всем, но как привыкнет, станет очень преданным.",
+        "name": "Топаз",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/15-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/14.jpg",
+        "description": "Трёхлапой Зайке пока не везло. Но мы надеемся, в её жизни скоро появится настоящий Человек. Ведь ей не хватает не лапы, ей не хватает любви и хозяина.",
+        "name": "Зайка",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/14-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/10.jpg",
+        "description": "Мы всегда подберём вам щенка. При поступлении в приют все они проходят карантин, обрабатываются от паразитов и прививаются, только после этого мы им подыскиваем дом.",
+        "name": "Щенки",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/10-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/9.jpg",
+        "description": "Лана, как настоящая модель, стройная, высокая и длинноногая молодая леди. Ласковая, добрая и не обделённая интеллектом!",
+        "name": "Лана",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/9-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/7.jpg",
+        "description": "Маня — отличный вариант для пожилого человека. Не суетливая, спокойная и покладистая собака. А то, что она будет преданна на 100%, так это даже не сомневайтесь.",
+        "name": "Маня",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/7-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/6.jpg",
+        "description": "Десино расположение нужно заслужить. Но это не сложно, ведь она так любит поесть. Чужие люди её не очень интересуют, а вот своему человеку она будет преданна до конца.",
+        "name": "Деся",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/6-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/5.jpg",
+        "description": "Сима — «мини-овчарка». Наредкость красивая и пропорциональная собака. А какая ласковая и добрая, так это не передать! Приезжайте знакомиться и убедитесь сами.",
+        "name": "Сима",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/5-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/4.jpg",
+        "description": "Банда она и есть банда. Энергия и позитив (хоть и чуток с примесью хулиганства). В тоже время очень понятлива и даже в приюте умудрилась выучить некоторые команды. Умница!",
+        "name": "Банда",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/4-480x300.jpg"
+    },
+    {
+        "img": "http://dobroe.perm.ru/wp-content/uploads/2016/10/FJsTvD8rUqg.jpg",
+        "description": "Пальма — очаровательная, некрупная, сообразительная и спокойная собачка. Отличный вариант для тех, кто хочет собаку на диван.",
+        "name": "Пальма",
+        "imgPrev": "http://dobroe.perm.ru/wp-content/uploads/2016/10/FJsTvD8rUqg-480x300.jpg"
+    }
+]
